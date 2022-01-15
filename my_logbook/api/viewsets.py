45dc970a9736e09377a87a031649793e052b0d6e
renan <renanhc96@gmail.com>
2022-01-15 18:12:31 -0300

    Add OAuth protocol

--- my_logbook/api/viewsets.py
+++ my_logbook/api/viewsets.py
@@ -1,16 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+
 from app.models import Transaction, Store
-from .serializers import TransactionSerializer, StoreSerializer
-from rest_framework import permissions
+from api.serializers import TransactionSerializer, StoreSerializer
+
 
 
 class StoreViewset(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [SessionAuthentication, OAuth2Authentication]
+    permission_classes = [IsAuthenticated]
 
 
 class TransactionsViewset(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [SessionAuthentication, OAuth2Authentication]
+    permission_classes = [IsAuthenticated]
--- my_logbook/my_logbook/settings.py
+++ my_logbook/my_logbook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'oauth2_provider',
     'corsheaders'
 ]
 
@@ -98,12 +99,14 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )    
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
